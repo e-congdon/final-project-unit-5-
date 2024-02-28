a3619c2df4cd57c710f43a5f4cedfca8b88cc4f5
lucie-luna <161515860+lucie-luna@users.noreply.github.com>
2024-02-28 13:27:47 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,22 @@
-function Spawn_enemy () {
+namespace SpriteKind {
+    export const treasure = SpriteKind.create()
+}
+function Spawn_Enemy () {
 	
 }
+function Spawn_Treasure () {
+    treasure = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.treasure)
+}
+let treasure: Sprite = null
 let blue_girl = sprites.create(img`
     . . . . . f f f f f . . . . 
     . . . . f 9 9 6 6 1 f . . . 
